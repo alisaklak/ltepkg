f5fe54fff0a0403bd624de33bdd06139b37b48f5
alisaklak <alisaklak@gmail.com>
2022-01-07 20:28:06 +0300

    nav head

--- resources/stubs/views/layouts/app.blade.php
+++ resources/stubs/views/layouts/app.blade.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -42,10 +38,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
   @include('sections.footer')
 </div>
 <!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
 <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
